5d2d9cc35339e3e6c4bf7db506c14424770d4ac6
Dipson <Dipsonbudhamagar2007@gmail.com>
2026-03-13 05:42:18 -0400

    Use unicode escape for smile emoji to fix encoding issue

--- widget/widget.js
+++ widget/widget.js
@@ -30,7 +30,7 @@
   // ---------------------------------------------------------------------------
   // SVG icons
   // ---------------------------------------------------------------------------
-  const TOOTH_SVG = `<span style="font-size:20px;line-height:1;">😁</span>`;
+  const TOOTH_SVG = `<span style="font-size:20px;line-height:1;">\u{1F601}</span>`;
 
   const CHAT_SVG = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" width="24" height="24">
     <path d="M20 2H4C2.9 2 2 2.9 2 4V22L6 18H20C21.1 18 22 17.1 22 16V4C22 2.9 21.1 2 20 2ZM20 16H5.2L4 17.2V4H20V16Z"/>
